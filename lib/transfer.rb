3d52997204cc56e1dd2cd826317ada5d5f5f7245
Adam Priest <sepone229@gmail.com>
2020-04-01 17:54:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,18 +1,14 @@
 class Transfer
-  
   attr_accessor :sender, :receiver, :status, :amount
-  
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
     @amount = amount
   end
-  
   def valid?
     @sender.valid? && @receiver.valid? ? true : false
   end
-  
   def execute_transaction
     if valid? && @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
@@ -23,7 +19,6 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
-  
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
